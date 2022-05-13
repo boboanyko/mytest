98eafbc39b52ce62d9f4b91c1b505785cd124fcc
boboanyko <akb_java@126.com>
2022-05-13 20:03:18 +0800

    add some zuul function

---
+++ src/main/java/com/mytest/local/BinAugTest.java
@@ -0,0 +1,12 @@
+package com.mytest.local;
+
+public class BinAugTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(1 << 4);
+        int roles = 1;
+        System.out.println(16 | 2);
+    }
+
+}
